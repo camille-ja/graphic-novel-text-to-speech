183bf4f1a514cad629360f942385786c82311b32
Camille James <james2@southwestern.edu>
2024-08-08 20:44:04 -0500

    added files

--- reader.py
+++ reader.py
@@ -3,13 +3,17 @@ import pyttsx3
 
 print("Start")
 
-image_path = "percy.jpg"
 engine = pyttsx3.init()
-reader = easyocr.Reader(['en'], gpu = False) #en = english, there's no gpu (i checked :/)
-results = reader.readtext(image_path) #creates a list with words, location of word, and confidence values
 
-for detection in results: #as long as there's something in the results list- so there's text that's being read
-    text = detection[1] #grabbing text
-    print(text)
-    engine.say(text)
-    engine.runAndWait()
+
+
+def read(img):
+    reader = easyocr.Reader(['en'], gpu = False) 
+    results = reader.readtext(img) #creates a list with words, location of word, and confidence values
+    for detection in results: #as long as there's something in the results list- so there's text that's being read
+        text = detection[1] #grabbing text
+        print(text)
+        #engine.say(text)
+        engine.runAndWait()
+
+read("percy.jpg")
